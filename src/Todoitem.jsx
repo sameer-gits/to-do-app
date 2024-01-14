7bdbf1cac88d0d054ea62a97356ded05bf694a6f
sameer-gits <ms3054825@gmail.com>
2024-01-14 19:08:23 +0530

    done

--- src/Todoitem.jsx
+++ src/Todoitem.jsx
@@ -28,16 +28,12 @@ export const TodoItem = ({ completed, id, title, toggleTodo, deleteTodo }) => {
             onChange={(e) => toggleTodo(id, e.target.checked)}
           />
           <p className={`align-middle font-semibold`}>{title}</p>
-          <img
-            className="w-32 h-32 absolute right-2 z-10"
-            src="./assets/Post1.jpg"
-          />
         </label>
 
         {isDeleting ? (
           <img
             className="w-32 h-32 absolute right-2 z-10"
-            src="./assets/Post1.jpg"
+            src="public/burst.gif"
           />
         ) : (
           <button
